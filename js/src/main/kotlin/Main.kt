c6d5fd967d78adebe29860f47de9ab0904ebda11
SIY1121 <sota.0512.0213@gmail.com>
2020-07-25 04:28:29 +0900

    add: http request for web

--- js/src/main/kotlin/Main.kt
+++ js/src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
 import space.siy.kotlinmultiplatformchat.createApplicationScreenMessage
+import space.siy.kotlinmultiplatformchat.fetchRemoteMessage
 import kotlin.browser.document
 
 fun main() {
     document.writeln(createApplicationScreenMessage())
+    fetchRemoteMessage {
+        document.writeln(it)
+    }
 }
