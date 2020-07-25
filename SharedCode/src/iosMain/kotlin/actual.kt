a560773bd298171f76fd3ed474ade66aefaedbfe
SIY1121 <sota.0512.0213@gmail.com>
2020-07-25 22:35:21 +0900

    add: websocket client for iOS

--- SharedCode/src/iosMain/kotlin/actual.kt
+++ SharedCode/src/iosMain/kotlin/actual.kt
@@ -1,6 +1,16 @@
 package space.siy.kotlinmultiplatformchat
 
+import io.ktor.client.HttpClient
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import kotlinx.coroutines.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
 import platform.UIKit.UIDevice
 import platform.darwin.dispatch_async
 import platform.darwin.dispatch_get_global_queue
@@ -34,4 +44,39 @@ actual fun ioDispatcher(): CoroutineDispatcher {
 
 actual fun mainDispatcher(): CoroutineDispatcher {
     return MainDispatcher
+}
+
+actual class Sender actual constructor(val host: String, val port: Int) {
+    actual var onMessage: (suspend (Message) -> Unit)? = null
+    actual var onReady: (suspend () -> Unit)? = null
+    val json = Json(JsonConfiguration.Stable)
+
+    private val client = HttpClient {
+        install(JsonFeature) {
+            serializer = KotlinxSerializer()
+        }
+    }
+
+    actual fun setup() {
+        GlobalScope.launch(ioDispatcher()) {
+            onReady?.invoke()
+            var received = 0
+            while (true) {
+                val res = client.get<Array<Message>>("http://$host:$port/msg?offset=$received")
+                res.forEach {
+                    onMessage?.invoke(it)
+                }
+                received += res.size
+            }
+        }
+    }
+
+    actual fun send(msg: Message) {
+        GlobalScope.launch(ioDispatcher()) {
+            client.post<String>("http://$host:$port/msg") {
+                contentType(ContentType.Application.Json)
+                body = msg
+            }
+        }
+    }
 }
